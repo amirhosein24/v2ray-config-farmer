1f1aa7424015dd88aa28bc12e6842e66d52945a0
garshaspx <garshaspx@gmail.com>
2024-04-04 22:15:53 +0330

    auto commit for configs

--- sublink.py
+++ sublink.py
@@ -40,12 +40,33 @@ def zout():
         try:
 
             repo = Repo(home)
+
+            # Stage all changes
             repo.git.add(".")
+
+            # Commit changes
             repo.index.commit("auto commit for configs")
+
+            # Set user configuration (This might need to be done globally or outside the script if it doesn't work)
             repo.git.config('user.name', git_creds[0])
             repo.git.config('user.email', f'{git_creds[0]}@gmail.com')
-            repo.remote(name='origin').set_url(f'https://github.com/{git_creds[0]}/v2ray-config-farmer.git')
-            repo.remote('origin').push(credentials=repo.git.credentials.PersonalAccessToken(git_creds[1]))
+
+            # Set up remote with credentials in the URL for the push
+            remote_url = f'https://{git_creds[0]}:{git_creds[1]}@github.com/{git_creds[0]}/v2ray-config-farmer.git'
+
+            # Check if the remote exists
+            remote_exists = False
+            for remote in repo.remotes:
+                if remote.name == "origin":
+                    remote_exists = True
+                    remote.set_url(remote_url)
+                    break
+
+            if not remote_exists:
+                repo.create_remote('origin', remote_url)
+
+            # Push changes
+            repo.git.push("origin", "HEAD:master", with_extended_output=True)
         except Exception as e:
             log.addlog(str(e), "sublink-gitpusher")
 
