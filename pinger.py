ac7a841228ef3c92223d97ceb89ea461cc5f82da
garshasp <garshaspx@gmail.com>
2024-04-20 15:24:36 +0330

    basic pinger added, requirements fixed

--- pinger.py
+++ pinger.py
@@ -1,12 +1,25 @@
 # i dont know what to doooooooooooooooooooooooooooooooooooooo help ╰（‵□′）╯
 #    it is supposed to get a v2ray url and check real time delay and connectivity and return true of false
 
+import socket
 
+def check_connection(url):
+    try:
+        
+        if url.startswith(('vless', "trojan")):
+            data = url.split("@")[1].split('?')[0]
+            ip, port = data.split(':')
+            
+        elif url.startswith('ss'):
+            data = url.split("@")[1].split('#')[0]
+            ip, port = data.split(':')
 
+        elif url.startswith('vmess'):
+            return False #TODO
 
+        sock = socket.create_connection((ip, port), timeout=5)
+        sock.close()
 
-
-
-
-
-
+        return True
+    except:
+        return False
--- sublink.py
+++ sublink.py
@@ -2,42 +2,39 @@
 # -*- coding: utf-8 -*-
 
 import log
+import pinger
+from creds import git_creds
 
 from git import Repo
 from time import sleep
 from base64 import b64encode
 from os import path
 from json import load
-from creds import git_creds
+
+home = path.dirname(path.abspath(__file__))
+
 
 def encoder(text):
     text = text.encode('utf-8')
     encoded_data = b64encode(text).decode('utf-8')
     return encoded_data
-home = path.dirname(path.abspath(__file__))
+
 
 def zout():
     while True:
-
-
-        with open(home + "/configs.json", encoding='utf-8') as file:
-            config_list = load(file)["configs"][::-1]
-
-        """
-                todo
-                pinger should be here 
-                it is supposed to get a v2ray config and return the real time daley of it (ping) 
-                and so the app can remove the bad configs
-        """
-
-        main = ""
-        for config in config_list:
-                main += config + "\n"
-
-        with open(home+"/zout.txt", "w") as file:
-            file.write(encoder(main))
-
         try:
+
+            with open(home + "/configs.json", encoding='utf-8') as file:
+                config_list = load(file)["configs"][::-1]
+
+            main = ""
+            for config in config_list:
+                if pinger.check_connection(config): # checks the connection of the config
+                    main += config + "\n"
+
+            with open(home+"/zout.txt", "w") as file:
+                file.write(encoder(main))
+
             repo = Repo(home)
             repo.git.add(".")
             repo.index.commit("auto commit for v2ray sub")
@@ -47,10 +44,11 @@ def zout():
             origin = repo.remote(name="origin")
             origin.set_url(remote_url)
             origin.push()
+
         except Exception as e:
             log.addlog(str(e), "sublink-gitpusher")
 
-        sleep(3600 * 1) # sleep for n hours and then start the first upload
+        sleep(3600 * 1) # sleep for n hours and then push the new configs to github
 
 
 from threading import Thread
